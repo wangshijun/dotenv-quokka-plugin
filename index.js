8349d510349d07cf0ba16e12d40514380fb5a05e
wangshijun <wangshijun2010@gmail.com>
2022-08-29 07:27:54 +0800

    feat: use dotenv-flow and detect env path in a smart way

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
 module.exports = {
   before: (config) => {
-    const dotenv = require('dotenv');
     const path = require('path');
+    const dotenv = require('dotenv-flow');
+    const findUp = require('findup-sync');
 
+    // see https://www.npmjs.com/package/dotenv-flow for all supported options
     const pluginConfig = config.dotenv || {};
     if (pluginConfig.path) {
       if (path.isAbsolute(pluginConfig.path)) {
-        dotenv.config({ path: pluginConfig.path });
+        dotenv.config({ ...pluginConfig, path: pluginConfig.path });
       } else {
-        dotenv.config({ path: path.join(process.cwd(), pluginConfig.path) });
+        dotenv.config({ ...pluginConfig, path: path.join(process.cwd(), pluginConfig.path) });
       }
     } else {
-      dotenv.config();
+      // detect dotenv location automatically form current file
+      const envPath = findUp('.env', { cwd: path.dirname(config.filePath) });
+      dotenv.config({ ...pluginConfig, path: envPath ? path.dirname(envPath) : process.cwd() });
     }
   },
 };
